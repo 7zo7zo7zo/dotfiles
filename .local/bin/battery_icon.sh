06e87f02e598486e0fea68ac425ad5ff8f219ff5
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-07-12 15:54:24 -0700

    Update battery_icon.sh

--- .local/bin/battery_icon.sh
+++ .local/bin/battery_icon.sh
@@ -1,43 +1,44 @@
 #!/bin/sh
+
 BAT=$(cat /sys/class/power_supply/BAT0/capacity)
 STAT=$(cat /sys/class/power_supply/BAT0/status)
-if [ $STAT = 'Charging' ]; then	
-	if [ $BAT -ge 90 ]; then
-		printf '';
-	elif [ $BAT -ge 80 ]; then
-		printf '';
-	elif [ $BAT -ge 60 ]; then
-		printf '';
-	elif [ $BAT -ge 40 ]; then
-		printf '';
-	elif [ $BAT -ge 30 ]; then
-		printf '';
+if [ $STAT = 'Charging' ] ; then	
+	if [ $BAT -ge 90 ] ; then
+		printf ''
+	elif [ $BAT -ge 80 ] ; then
+		printf ''
+	elif [ $BAT -ge 60 ] ; then
+		printf ''
+	elif [ $BAT -ge 40 ] ; then
+		printf ''
+	elif [ $BAT -ge 30 ] ; then
+		printf ''
 	else
-		printf '';
+		printf ''
 	fi
 else
-	if [ $BAT -eq 100 ]; then
-		printf '';
-	elif [ $BAT -ge 90 ]; then
-		printf '';
-	elif [ $BAT -ge 80 ]; then
+	if [ $BAT -eq 100 ] ; then
+		printf ''
+	elif [ $BAT -ge 90 ] ; then
+		printf ''
+	elif [ $BAT -ge 80 ] ; then
 		printf '';
-	elif [ $BAT -ge 70 ]; then
-		printf '';
-	elif [ $BAT -ge 60 ]; then
-		printf '';
-	elif [ $BAT -ge 50 ]; then
-		printf '';
-	elif [ $BAT -ge 40 ]; then
-		printf '';
-	elif [ $BAT -ge 30 ]; then
-		printf '';
-	elif [ $BAT -ge 20 ]; then
-		printf '';
-	elif [ $BAT -ge 10 ]; then
-		printf '';
+	elif [ $BAT -ge 70 ] ; then
+		printf ''
+	elif [ $BAT -ge 60 ] ; then
+		printf ''
+	elif [ $BAT -ge 50 ] ; then
+		printf ''
+	elif [ $BAT -ge 40 ] ; then
+		printf ''
+	elif [ $BAT -ge 30 ] ; then
+		printf ''
+	elif [ $BAT -ge 20 ] ; then
+		printf ''
+	elif [ $BAT -ge 10 ] ; then
+		printf ''
 	else
-		printf '';
+		printf ''
 	fi
 fi
 printf " $BAT%%"
