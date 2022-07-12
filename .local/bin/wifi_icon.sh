040d80462f37de4b88f74990b937cc7473aff35e
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-07-12 15:57:48 -0700

    Update wifi_icon.sh

--- .local/bin/wifi_icon.sh
+++ .local/bin/wifi_icon.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ $(cat /sys/class/net/eth0/operstate) == 'up' ] ; then
+if [ $(cat /sys/class/net/eth0/operstate) == 'up' ]; then
 	printf ''
-elif [ $(cat /sys/class/net/wlan0/operstate) == 'up' ] ; then
+elif [ $(cat /sys/class/net/wlan0/operstate) == 'up' ]; then
 	printf '直'
 else
 	printf '睊'
