93b2dd2a650fd5b1273ed03bef2d02496849af17
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-07-13 01:57:32 -0700

    Update plugins.lua

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,10 +1,13 @@
 local packer = require 'packer'
 
 return packer.startup(function(use)
-	use "wbthomason/packer.nvim"
+  use "wbthomason/packer.nvim"
   use "hrsh7th/nvim-cmp"
-	use "hrsh7th/cmp-nvim-lsp"
-	use "neovim/nvim-lspconfig"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "neovim/nvim-lspconfig"
   use "hrsh7th/cmp-buffer"
   use "hrsh7th/cmp-path"
+  use "L3MON4D3/LuaSnip"
+  use "saadparwaiz1/cmp_luasnip"
+  use "rafamadriz/friendly-snippets"
 end)
