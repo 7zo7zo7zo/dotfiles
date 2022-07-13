1cd1f0624ebc953969aefa401cc4bcaeb2dd0db4
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-07-13 02:13:26 -0700

    Update cmp.lua

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -3,6 +3,7 @@ local has_words_before = function()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
+local luasnip = require("luasnip")
 local cmp = require 'cmp'
 
 cmp.setup {
@@ -19,45 +20,35 @@ cmp.setup {
 
     ['<C-e>'] = cmp.mapping.abort(),
 
-    ['<Tab>'] = function(fallback)
-      if not cmp.select_next_item() then
-        if vim.bo.buftype ~= 'prompt' and has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
       end
-    end,
+    end, { "i", "s" }),
 
-    ['<S-Tab>'] = function(fallback)
-      if not cmp.select_prev_item() then
-        if vim.bo.buftype ~= 'prompt' and has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
       end
-    end,
+    end, { "i", "s" }),
   },
 
   snippet = {
     expand = function(args)
-      local line_num, col = unpack(vim.api.nvim_win_get_cursor(0))
-      local line_text = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)[1]
-      local indent = string.match(line_text, '^%s*')
-      local replace = vim.split(args.body, '\n', true)
-      local surround = string.match(line_text, '%S.*') or ''
-      local surround_end = surround:sub(col)
-
-      replace[1] = surround:sub(0, col - 1)..replace[1]
-      replace[#replace] = replace[#replace]..(#surround_end > 1 and ' ' or '')..surround_end
-      if indent ~= '' then
-        for i, line in ipairs(replace) do
-          replace[i] = indent..line
-        end
-      end
-
-      vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, replace)
+      luasnip.lsp_expand(args.body)
     end,
   },
 }
+
+--require("luasnip.loaders.from_vscode").load({ paths = { "~/.local/share/nvim/site/pack/packer/start/friendly-snippets" } })
+require("luasnip.loaders.from_vscode").lazy_load()
