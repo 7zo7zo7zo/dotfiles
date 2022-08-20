f687f57963ebab4423a3cedaa193d93947d57c79
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-08-19 21:12:02 -0700

    Update mic.sh

--- .local/bin/mic.sh
+++ .local/bin/mic.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ $(pacmd list-sources | awk '/muted/ { print $2 }') == *'yes'* ]]; then
-  if [[ $(pacmd list-sink-inputs) != "0 sink input(s) available." ]]; then
+if [ $(pactl get-source-input @DEFAULT_SOURCE@ | awk '/muted/ { print $2 }') == 'yes' ]; then
+  if [ $(pactl list | grep -c RUNNING) -gt 2 ]; then
     echo Trying to use! 
   else 
     echo 
   fi
-elif [[ $(pacmd list-sink-inputs) != '0 sink input(s) available.' ]]; then
+elif [ $(pactl list | grep -c RUNNING) -gt 2 ]; then
   echo In use! 
 else
   echo 
