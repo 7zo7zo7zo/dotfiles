a518ce2c7c7f6ba69468006e29f506b8eb6e58fd
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2023-03-31 01:26:59 -0700

    Add files via upload

---
+++ .local/bin/wacom.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+STYLUS=$(xsetwacom list devices | grep STYLUS | awk '{print $7}')
+WIDTH=$(xsetwacom get $STYLUS Area | awk '{print $3}')
+SCALED_HEIGHT=$(expr $WIDTH \* 1080 / 1920)
+echo $WIDTH
+echo $SCALED_HEIGHT
+xsetwacom set $STYLUS Area 0 0 $WIDTH $SCALED_HEIGHT
