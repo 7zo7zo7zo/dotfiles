3300c8313113100a843c87e859ebf7cf86769de8
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2023-03-31 10:25:51 -0700

    Update wacom.sh

--- .local/bin/wacom.sh
+++ .local/bin/wacom.sh
@@ -2,6 +2,4 @@
 STYLUS=$(xsetwacom list devices | grep STYLUS | awk '{print $7}')
 WIDTH=$(xsetwacom get $STYLUS Area | awk '{print $3}')
 SCALED_HEIGHT=$(expr $WIDTH \* 1080 / 1920)
-echo $WIDTH
-echo $SCALED_HEIGHT
 xsetwacom set $STYLUS Area 0 0 $WIDTH $SCALED_HEIGHT
